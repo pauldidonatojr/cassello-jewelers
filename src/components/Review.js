1c0a2a8b115f68748151fbcc5756ed82d5730243
RajaAliHassan1 <60552815+RajaAliHassan1@users.noreply.github.com>
2023-08-19 00:13:39 +0500

    update

--- src/components/Review.js
+++ src/components/Review.js
@@ -5,6 +5,8 @@ import styled from "styled-components";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import Rating from '@mui/material/Rating';
+import StarIcon from '@mui/icons-material/Star';
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
@@ -14,6 +16,7 @@ function Reviews() {
       Image:
         "https://images.pexels.com/photos/3628700/pexels-photo-3628700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       Name: "Evelyn M.",
+      Star:"3.5",
       Review:
         "Absolutely mesmerized by the exquisite craftsmanship and unique designs at this jewelry store. Every piece seems to tell a story, and I couldn't resist treating myself to a stunning necklace. I'm already planning my next visit!",
     },
@@ -21,6 +24,7 @@ function Reviews() {
       Image:
         "https://images.pexels.com/photos/1674666/pexels-photo-1674666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       Name: "James R.",
+      Star:"4.5",
       Review:
         "This store is a hidden gem! Pun intended. The staff is incredibly knowledgeable and patient. I needed assistance selecting the perfect engagement ring, and they helped me find a piece that captured the essence of our love. My fiancée was beyond thrilled!",
     },
@@ -28,6 +32,7 @@ function Reviews() {
       Image:
         "https://images.pexels.com/photos/1181424/pexels-photo-1181424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       Name: "Sophia A.",
+      Star:"5",
       Review:
         "A jewelry wonderland that brings dreams to life! I fell in love with their vintage collection. The attention to detail and the quality of the gems are unmatched. I feel like a queen whenever I wear the ring I purchased from here.",
     },
@@ -35,6 +40,7 @@ function Reviews() {
       Image:
         "https://images.pexels.com/photos/12118422/pexels-photo-12118422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       Name: "Oliver H.",
+      Star:"4.5",
       Review:
         "Impressed by the modern and sleek designs at this store. Their minimalist approach to jewelry is captivating. The rose gold bracelet I bought is delicate yet eye-catching, making it my new everyday accessory.",
     },
@@ -42,6 +48,7 @@ function Reviews() {
       Image:
         "https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       Name: "Isabella C.",
+      Star:"2.5",
       Review:
         "What a delightful experience! The store's ambiance is so elegant and inviting. I bought a pair of diamond studs, and they added a touch of sophistication to my collection. The staff's warm service made the visit even more special.",
     },
@@ -50,6 +57,7 @@ function Reviews() {
         "https://images.pexels.com/photos/3628700/pexels-photo-3628700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
       Name: "William P.",
+      Star:"3",
       Review:
         "Talk about exceptional customer service! They went above and beyond to create a custom necklace for my anniversary. The final piece exceeded my expectations, and my wife was truly moved by the thoughtful gift.",
     },
@@ -58,6 +66,7 @@ function Reviews() {
         "https://images.pexels.com/photos/1674666/pexels-photo-1674666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
       Name: "Ava S.",
+      Star:"4",
       Review:
         "Quality and affordability hand in hand. I was amazed by the range of options available without compromising on craftsmanship. My daughter's birthstone pendant was not only beautiful but also reasonably priced.",
     },
@@ -66,6 +75,7 @@ function Reviews() {
         "https://images.pexels.com/photos/1181424/pexels-photo-1181424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
       Name: "Liam G.",
+      Star:"4.5",
       Review:
         "An ode to elegance! This store celebrates timeless beauty with its classical designs. I purchased a pearl necklace that adds a touch of grace to any outfit. It's a symbol of enduring style.",
     },
@@ -74,6 +84,7 @@ function Reviews() {
         "https://images.pexels.com/photos/12118422/pexels-photo-12118422.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
       Name: "Harper L.",
+      Star:"5",
       Review:
         "A treasure trove for unique jewelry enthusiasts. I adore their fusion of traditional and contemporary designs. The opal ring I bought is a conversation starter, and I'm already planning to explore their other collections.",
     },
@@ -82,6 +93,7 @@ function Reviews() {
         "https://images.pexels.com/photos/3628700/pexels-photo-3628700.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 
       Name: "Emma K.",
+      Star:"4",
       Review:
         "Absolutely enchanted by the ethereal pieces at this store. The sapphire bracelet I acquired has an almost magical allure. It's more than jewelry; it's wearable art that brings joy with every glance.",
     },
@@ -128,6 +140,11 @@ function Reviews() {
                       </div>
                     </Grid>
                     <Grid item lg={12}>
+                      <div className="RatingHolder">
+                      <Rating className="Rating" name="half-rating-read" defaultValue={review.Star} precision={0.5} readOnly />
+                       </div>
+                    </Grid>
+                    <Grid item lg={12}>
                       <div>
                         <p className="ReviewName">{review.Name}</p>
                       </div>
@@ -160,6 +177,10 @@ font-family: "Century Gothic", sans-serif;
     justify-content: center;
     align-items: center;
   }
+  .RatingHolder{
+    width:100%;
+    display:flex;
+  }
   .ImageHolder {
     display: flex;
     justify-content: center;
@@ -181,9 +202,10 @@ font-family: "Century Gothic", sans-serif;
   .ReviewName{
 
     text-align:left;
+    padding-left:0.5%;
   }
   .ReviewText {
-    font-size: 35px;
+    font-size: 30px;
     font-weight: 10;
     text-align:left;
   }
